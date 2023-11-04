d8cb51c0d86022f155a63e1da9a93678dd54da36
Craig <11655707+CodeZeno@users.noreply.github.com>
2023-11-04 14:09:50 +1000

    Revert forge agent.py

--- autogpts/forge/forge/agent.py
+++ autogpts/forge/forge/agent.py
@@ -6,13 +6,12 @@ from forge.sdk import (
     StepRequestBody,
     Task,
     TaskRequestBody,
-    Workspace,
-    PromptEngine,
-    chat_completion_request,
-    ChromaMemStore
+    Workspace,    
+    PromptEngine,	
+    chat_completion_request,	
+    ChromaMemStore	
 )
-from .sdk import PromptEngine
-import json
+import json	
 import pprint
 
 LOG = ForgeLogger(__name__)
@@ -123,72 +122,26 @@ class ForgeAgent(Agent):
         multiple steps. Returning a request to continue in the step output, the user can then decide
         if they want the agent to continue or not.
         """
-
-	# Get this step's task from the database
-        task = await self.db.get_task(task_id)
-
-	# Create a new step in the database
+        # An example that
         step = await self.db.create_step(
             task_id=task_id, input=step_request, is_last=True
         )
 
-	# Log the message
-    	LOG.info(f"\t✅ Final Step completed: {step.step_id} input: {step.input[:19]}")
+        self.workspace.write(task_id=task_id, path="output.txt", data=b"Washington D.C")
 
-	# Initialise the prompt engine
-	prompt_engine = PromptEngine("gpt-3.5-turbo")
+        await self.db.create_artifact(
+            task_id=task_id,
+            step_id=step.step_id,
+            file_name="output.txt",
+            relative_path="",
+            agent_created=True,
+        )
 
-	# Load a prompt from a file
-	system_prompt = prompt_engine.load_prompt("system-format")
+        step.output = "Washington D.C"
 
-	# Initialize the messages list with the system prompt
-	messages = [
-		{"role": "system", "content": system_prompt},
-	]
+        LOG.info(f"\t✅ Final Step completed: {step.step_id}. \n" +
+                 f"Output should be placeholder text Washington D.C. You'll need to \n" +
+                 f"modify execute_step to include LLM behavior. Follow the tutorial " +
+                 f"if confused. ")
 
-	# Define the task parameters
-	task_kwargs = {
-    		"task": task.input,
-    		"abilities": self.abilities.list_abilities_for_prompt(),
-	}
-
-	# Load the task prompt with the defined task parameters
-	task_prompt = prompt_engine.load_prompt("task-step", **task_kwargs)
-
-	# Append the task prompt to the messages list
-    	messages.append({"role": "user", "content": task_prompt})
-
-	try:
-		# Define the parameters for the chat completion request
-		chat_completion_kwargs = {
-          		"messages": messages,
-          		"model": "gpt-3.5-turbo",
-      		}
-      		# Make the chat completion request and parse the response
-      		chat_response = await chat_completion_request(**chat_completion_kwargs)
-      		answer = json.loads(chat_response["choices"][0]["message"]["content"])
-
-      		# Log the answer for debugging purposes
-      		LOG.info(pprint.pformat(answer))
-
-	except json.JSONDecodeError as e:
-		# Handle JSON decoding errors
-		LOG.error(f"Unable to decode chat response: {chat_response}")
-	except Exception as e:
-		# Handle other exceptions
-		LOG.error(f"Unable to generate chat response: {e}")Step 7: Executing the Derived Ability
-
-	# Extract the ability from the answer
-    	ability = answer["ability"]
-
-    	# Run the ability and get the output
-    	# We don't actually use the output in this example
-    	output = await self.abilities.run_ability(
-        	task_id, ability["name"], **ability["args"]
-    	)
-
-    	# Set the step output to the "speak" part of the answer
-    	step.output = answer["thoughts"]["speak"]
-
-	# Return the completed step
-    	return step
+        return step
