d7a7b98c4b95c7528fa8ac2eef2a058de09f82a3
Craig Constable <craig@codezeno.com.au>
2023-11-08 16:23:21 +1000

    Susan v0.04
    Updating version.

--- CommitPush.cmd
+++ CommitPush.cmd
@@ -1,5 +1,5 @@
-for %%* in (.) do SET version=%%~nx* v0.03
-SET comment=Now saving agent responses.
+for %%* in (.) do SET version=%%~nx* v0.04
+SET comment=Updating version.
 git add -A
 git commit -a -m "%version%" -m "" -m "%comment%"
 git push
